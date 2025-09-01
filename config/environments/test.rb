66c7d1ec17045ae9fc6996db246272aeb1805425
jonatas venancio <jvenancio167@hotmail.com>
2025-09-01 00:55:15 -0300

    fix: disable host authorization and ssl in test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,4 +61,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  
+  # Disable host authorization completely in test environment
+  config.force_ssl = false
+  config.hosts = nil
 end
