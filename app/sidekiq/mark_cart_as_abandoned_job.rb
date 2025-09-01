e289aa5c5d37963177a0394e1448c6e2388da1a9
jonatas venancio <jvenancio167@hotmail.com>
2025-09-01 00:55:48 -0300

    feat: implement cart abandonment job

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,32 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+  def perform(cart_id)
+    cart = Cart.find_by(id: cart_id)
+    return unless cart && !cart.abandoned?
+
+    cart.update!(
+      abandoned: true,
+      abandoned_at: Time.current
+    )
+
+    event_store.publish(
+      CartAbandoned.new(data: {
+        cart_id: cart.id,
+        session_id: cart.session_id,
+        total_items: cart.cart_items.sum(:quantity),
+        total_value: cart.total_price,
+        last_interaction_at: cart.updated_at,
+        abandoned_at: cart.abandoned_at
+      })
+    )
+
+    Rails.logger.info "Carrinho #{cart_id} marcado como abandonado"
+  end
+
+  private
+
+  def event_store
+    Rails.configuration.event_store
   end
 end
