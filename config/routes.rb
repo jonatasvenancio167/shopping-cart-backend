e66583345bf8cb9a6950b970bc4413df7730c242
jonatas venancio <jvenancio167@hotmail.com>
2025-09-01 00:48:28 -0300

    feat(cart): add routes for cart operations

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+  
+  # Cart routes
+  post '/cart/add_item', to: 'carts#add_item'
+  delete '/cart/:product_id', to: 'carts#remove_item'
+  post '/cart', to: 'carts#create'
+  get '/cart', to: 'carts#show'
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
